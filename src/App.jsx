e60fee7e07b3ed4508b3daa0769fb1eced100b7d
nidhipawar5 <nidhipawar0504@gmail.com>
2024-01-07 17:03:44 +0530

    login function complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 import Login from './Login'
+import { getTokenFromResponse } from './spotify'
 
 const App = () => {
+  
+  const [token,setToken]=useState(null)  
+
+  useEffect(() => {
+    const hash = getTokenFromResponse()
+    window.location.hash =""
+    const _token =hash.access_token
+    // console.log('I have a token=',_token)
+
+    if (_token){
+        setToken(_token)
+    }
+  },[])
+
   return (
     <div className='app'>
-        <Login />
+        {
+            token ? (
+             <h1>You are logged in.</h1>
+            ) : (
+                <Login />
+            )
+        }
+        
     </div>
   )
 }
